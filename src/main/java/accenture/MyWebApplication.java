7551a0749035cd2cef6ce60514cb01dea0d62217
mura1ik7ishna <mura1ik7ishna@gmail.com>
2020-01-03 12:27:44 +0530

    firt war file

---
+++ src/main/java/accenture/MyWebApplication.java
@@ -0,0 +1,16 @@
+package accenture;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.PerspectivesModule;
+
+public class MyWebApplication extends OrienteerWebApplication
+{
+	@Override
+	public void init()
+	{
+		super.init();
+		mountPackage("accenture.web");
+		registerModule(DataModel.class);
+	}
+	
+}
